5b88aa87061ae496e4068b8657e4fa46f565dac7
Dmytro Kozlovskyi <120205405+Dmytroukraine@users.noreply.github.com>
2023-06-01 11:37:18 +0300

    MF

--- src/index.js
+++ src/index.js
@@ -1,70 +1,113 @@
-import './sass/main.scss';
+import ImagesApiService from './js/api-pixabay';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
-// import 'notiflix/dist/notiflix-3.2.6.min.css';
-import ImagesAPIService from './js/APIService';
-import Markup from './js/markup';
 
-const response = await fetch('https://randomuser.me/api/', {
-  method: 'GET',
-  headers: {
-    // set Accept header to application/json
-    Accept: 'application/json',
-  },
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  imagesContainer: document.querySelector('.gallery'),
+  // loadMoreBtn: document.querySelector('.load-more'),
+  target: document.querySelector('.js-guard'),
+  theEnd: document.querySelector('.end'),
+};
+
+const imagesApiService = new ImagesApiService();
+
+refs.searchForm.addEventListener('submit', onFormSubmit);
+// refs.loadMoreBtn.addEventListener('click', onLoadMoreClick);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  imagesApiService.query = e.currentTarget.elements.searchQuery.value;
+  imagesApiService.resetPage();
+  imagesApiService.getImages().then(images => {
+    clearImagesContainer();
+    appendImagesMarkup(images);
+    observer.observe(refs.target);
+    if (images.totalHits === 0) {
+      Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+    }
+    gallery.refresh()
+  });
+}
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 150,
+  loop: false,
 });
 
-const formSearch = document.querySelector('.search-form');
-const galleryRef = document.querySelector('.gallery');
-const loadBtn = document.querySelector('.load-more');
+// function onLoadMoreClick(e) {
+//   imagesApiService.getImages().then(images => {
+//     appendImagesMarkup(images);
+//     gallery.refresh();
+//   });
+// }
 
-const imagesAPIService = new ImagesAPIService();
-const renderMarkup = new Markup({ selector: galleryRef });
+function appendImagesMarkup(images) {
+  const imageMarkup = images.hits
+    .map(
+      image => `<div class="photo-card">
+        <a class="gallery__link" href="${image.largeImageURL}">
+        <img 
+        class="gallery__image"
+        src="${image.webformatURL}" 
+        alt="${image.tags}" 
+        loading="lazy" 
+        width=300
+         heihgt=300 
+        />
+       </a>
+        <div class="info">
+            <p class="info-item">
+                <b>Likes</b>
+                ${image.likes}
+            </p>
+            <p class="info-item">
+                <b>Views</b>
+                ${image.views}
+            </p>
+            <p class="info-item">
+                <b>Comments</b>
+                ${image.comments}
+            </p>
+            <p class="info-item">
+                <b>Downloads</b>
+                ${image.downloads}
+            </p>
+        </div>
+    </div>`
+    )
+    .join('');
 
-formSearch.addEventListener('submit', onFormSearch);
-loadBtn.addEventListener('click', onLoadMore);
-
-async function onFormSearch(event) {
-  event.preventDefault();
-  renderMarkup.reset();
-  imagesAPIService.query = event.currentTarget.searchQuery.value.trim();
-
-  if (imagesAPIService.query === '') {
-    loadBtn.classList.add('is-hidden');
-    return Notiflix.Notify.info('Your query is empty. Try again!');
-  }
-  imagesAPIService.resetPage();
-
-  try {
-    loadBtn.classList.remove('is-hidden');
-    await fetchImages();
-  } catch (error) {
-    loadBtn.classList.add('is-hidden');
-    return Notiflix.Notify.failure(error.message);
-  }
-
-  formSearch.reset();
+  refs.imagesContainer.insertAdjacentHTML('beforeend', imageMarkup);
 }
 
-async function onLoadMore() {
-  try {
-    await fetchImages();
-  } catch {
-    return Notiflix.Notify.failure(error.message);
-  }
+function clearImagesContainer() {
+  refs.imagesContainer.innerHTML = '';
 }
 
-async function fetchImages() {
-  try {
-    loadBtn.disabled = true;
-    const images = await imagesAPIService.fetchImages();
-    renderMarkup.items = images;
-    renderMarkup.render();
-  } catch {
-    return Notiflix.Notify.failure(error.message);
-  }
+let options = {
+  root: null,
+  rootMargin: '300px',
+  threshold: 1.0,
+}
 
-  if (imagesAPIService.endOfHits) {
-    return loadBtn.classList.add('is-hidden');
-  }
-  loadBtn.disabled = false;
-  loadBtn.textContent = 'Load more';
+let observer = new IntersectionObserver(onLoad, options);
+
+function onLoad(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      imagesApiService.getImages().then(images => {
+    appendImagesMarkup(images);
+        gallery.refresh();
+        if (imagesApiService.currentHits > images.totalHits) {
+          observer.unobserve(refs.target);
+          refs.theEnd.classList.remove('is-hidden');
+        }
+  });
+    }
+  });
 }
---
+++ src/js/api-pixabay.js
@@ -0,0 +1,37 @@
+const axios = require('axios').default;
+
+const API_KEY = '36867238-d18d023007d9afe06dc91b3fb';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export default class ImagesApiService {
+  constructor() {
+      this.searchQuery = '';
+    this.page = 1;
+    this.currentHits = 0;
+  }
+
+  getImages() {
+    return axios
+      .get(
+        `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&safesearch=true&orientation=horyzontal&page=${this.page}&per_page=40`
+      )
+        .then(response => {
+          this.page += 1;
+          this.currentHits += response.data.hits.length;
+          console.log(this.currentHits);
+            return response.data;
+      });
+    }
+    
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+}
